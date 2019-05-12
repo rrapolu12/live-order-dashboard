99d7c64482be34711ffe4226769600cccdd8b3ee
rrapolu12 <rrapolu@outlook.com>
2019-05-12 20:52:28 +0100

    testing first section of registering order in the test with register test

--- src/main/java/org/ramrapolu/orders/dashboard/LiveOrderDashBoard.java
+++ src/main/java/org/ramrapolu/orders/dashboard/LiveOrderDashBoard.java
@@ -1,6 +1,18 @@
 package org.ramrapolu.orders.dashboard;
 
+import org.ramrapolu.orders.Order;
+
 import java.util.*;
 
 public class LiveOrderDashBoard {
+
+    private Set<Order> orders = new HashSet<>();
+
+    public void register(Order order) {
+        orders.add(order);
+    }
+
+    public boolean hasRegistered(Order order){
+        return orders.contains(order);
+    }
 }
--- src/test/java/org/ramrapolu/orders/dashboard/LiveOrderDashBoardTest.java
+++ src/test/java/org/ramrapolu/orders/dashboard/LiveOrderDashBoardTest.java
@@ -1,7 +1,11 @@
 package org.ramrapolu.orders.dashboard;
 
 import junitparams.JUnitParamsRunner;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ramrapolu.orders.Order;
 
 import static org.junit.Assert.*;
 
@@ -11,5 +15,23 @@ public class LiveOrderDashBoardTest {
     private static final double DECIMAL_POINT_ACCURACY = 1.0/Math.pow(10, 5);
 
     private LiveOrderDashBoard board;
+
+    private String USERNAME = "testuser";
+    private double quantity = 3.5;
+    private double price = 303.0;
+
+    private Order order = new Order(USERNAME,quantity,price);
+
+    @Before
+    public void before(){
+        board = new LiveOrderDashBoard();
+    }
+
+    @Test
+    public void test_register_order_on_dashboard(){
+        board.register(order);
+        boolean registered = board.hasRegistered(order);
+        Assert.assertTrue(registered);
+    }
 
 }
